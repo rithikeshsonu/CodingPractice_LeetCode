dbbb1d00f1b27f682fd6f41d67620d9493e85dbb
Rithikesh Surthi <36332866+rithikeshsonu@users.noreply.github.com>
2025-12-28 21:12:02 +0530

    Time: 1 ms (100.00%) | Memory: 59.7 MB (49.02%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 1) {
+                currentCount++;
+                maxCount = Math.Max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
