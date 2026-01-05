58744a64d97cf4be5f393b8a94131fc9f1173c75
Rithikesh Surthi <36332866+rithikeshsonu@users.noreply.github.com>
2026-01-05 23:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (55.60%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cs
+++ 88-merge-sorted-array/merge-sorted-array.cs
@@ -1,14 +1,26 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m-1;
-        int p2 = n-1;
-        int i = m+n-1;
-        while(p2>=0)
-        {
-            if(p1 >=0 && nums1[p1] > nums2[p2])
-                nums1[i--] = nums1[p1--];
-            else
-               nums1[i--]= nums2[p2--];
+                int i = m - 1;          // last valid index in nums1
+        int j = n - 1;          // last index in nums2
+        int k = m + n - 1;      // last index in nums1
+
+        // Merge from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // Copy remaining elements of nums2 (if any)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 }
