8dbfd929755dd71ff374eb2aadcea9a69c8def94
Rithikesh Surthi <36332866+rithikeshsonu@users.noreply.github.com>
2026-01-07 20:24:03 +0530

    Time: 43 ms (36.90%) | Memory: 70 MB (59.11%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+            HashSet<int> window = new HashSet<int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (window.Contains(nums[i]))
+        {
+            return true;
+        }
+
+        window.Add(nums[i]);
+
+        if (window.Count > k)
+        {
+            window.Remove(nums[i - k]);
+        }
+    }
+    return false;
+    }
+}
