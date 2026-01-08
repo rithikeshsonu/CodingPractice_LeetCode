9d64247b6ae633285699bb80ff6a1f35b2d8d844
Rithikesh Surthi <36332866+rithikeshsonu@users.noreply.github.com>
2026-01-08 22:48:50 +0530

    Time: 14 ms (24.75%) | Memory: 44.9 MB (6.44%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cs
+++ 414-third-maximum-number/third-maximum-number.cs
@@ -1,13 +1,16 @@
 public class Solution {
     public int ThirdMax(int[] nums) {
-        HashSet<int> topThree = new HashSet<int>();
-        foreach(int num in nums){
-            topThree.Add(num);
-            if(topThree.Count > 3){
-                topThree.Remove(topThree.Min());
-            }
-        }
-        if(topThree.Count < 3) return topThree.Max();
-        else return topThree.Min();
+        // HashSet<int> topThree = new HashSet<int>();
+        // foreach(int num in nums){
+        //     topThree.Add(num);
+        //     if(topThree.Count > 3){
+        //         topThree.Remove(topThree.Min());
+        //     }
+        // }
+        // if(topThree.Count < 3) return topThree.Max();
+        // else return topThree.Min();
+        nums = nums.Distinct().OrderByDescending(n => n).ToArray();
+        if(nums.Length >= 3) return nums[2];
+        return nums[0];
     }
 }
